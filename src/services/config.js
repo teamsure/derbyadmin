447ba75eee24c0a3278ba9d58b54822768b72ef3
Teamsure <teamsure@ninedots.com>
2022-12-10 01:06:36 +0530

    changed config.js for new derbyprints api server url

--- src/services/config.js
+++ src/services/config.js
@@ -1,3 +1,3 @@
 const _medusaUrl = __MEDUSA_BACKEND_URL__ || "http://localhost:9000"
-const medusaUrl = "https://derbyserver-production.up.railway.app"
+const medusaUrl = "https://derbyprintsserver-production.up.railway.app"
 export { medusaUrl }
