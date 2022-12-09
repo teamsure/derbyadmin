894c6ac26f0a06c4fefad98d8252bdc68e7a9dcf
Teamsure <teamsure@ninedots.com>
2022-12-09 21:46:43 +0530

    added the derbyserver url

--- src/services/config.js
+++ src/services/config.js
@@ -1,3 +1,3 @@
-const medusaUrl = __MEDUSA_BACKEND_URL__ || "http://localhost:9000"
-
+const _medusaUrl = __MEDUSA_BACKEND_URL__ || "http://localhost:9000"
+const medusaUrl = "https://derbyserver-production.up.railway.app"
 export { medusaUrl }
